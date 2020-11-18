6daf59988a03d67ac7c70422109003c89f15f6d7
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2020-11-18 14:54:21 +0200

    Implement buffer management
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- egl_shim.c
+++ egl_shim.c
@@ -13,10 +13,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include <gbm.h>
 
 #include <X11/Xutil.h>
+#include <xcb/xcb.h>
 #include <xcb/dri3.h>
 #include <xcb/present.h>
 #include <X11/Xlib-xcb.h>
@@ -206,8 +208,35 @@ eglGetConfigAttrib(EGLDisplay dpy, EGLConfig config, EGLint attribute,
   return EGL_TRUE;
 }
 
-struct gbm_surface *gbm_surface = 0;
+static struct gbm_surface *gbm_surface = 0;
 static EGLNativeWindowType _win;
+static xcb_special_event_t *special_ev = NULL;
+
+xcb_special_event_t *x_init_special_event_queue(xcb_connection_t *c, xcb_window_t window, uint32_t *special_ev_stamp)
+{
+  uint32_t id = xcb_generate_id(c);
+  xcb_void_cookie_t cookie;
+
+  cookie = xcb_present_select_input_checked(c, id, window,
+                                            XCB_PRESENT_EVENT_MASK_COMPLETE_NOTIFY |
+                                            XCB_PRESENT_EVENT_MASK_IDLE_NOTIFY |
+                                            XCB_PRESENT_EVENT_MASK_CONFIGURE_NOTIFY/* |
+                                                                                            XCB_PRESENT_EVENT_MASK_REDIRECT_NOTIFY*/);
+  xcb_generic_error_t *error =
+      xcb_request_check(c, cookie);
+  if (error)
+  {
+    printf("req xge failed\n");
+    return NULL;
+  }
+
+  xcb_special_event_t *special_ev = xcb_register_for_special_xge(c, &xcb_present_id, id, special_ev_stamp);
+
+  if (!special_ev)
+    printf("no special ev\n");
+
+  return special_ev;
+}
 
 EGLAPI EGLSurface EGLAPIENTRY
 eglCreateWindowSurface(EGLDisplay dpy, EGLConfig config,
@@ -227,7 +256,9 @@ eglCreateWindowSurface(EGLDisplay dpy, EGLConfig config,
                                    c->base.NativeVisualID,
                                    GBM_BO_USE_RENDERING | GBM_BO_USE_SCANOUT);
 
-  free (geom);
+  free(geom);
+
+  special_ev = x_init_special_event_queue(xcb_conn, win, NULL);
 
   egl_surface = _eglCreateWindowSurface(dpy, config,
                                         (EGLNativeWindowType)gbm_surface,
@@ -236,57 +267,221 @@ eglCreateWindowSurface(EGLDisplay dpy, EGLConfig config,
   return egl_surface;
 }
 
-static void
-destroy_pixmap_data(struct gbm_bo *bo, void *pixmap_data)
+enum buffer_status
 {
-  (void)bo;
-  free(pixmap_data);
+  BUFFER_STATUS_FREE,
+  BUFFER_STATUS_PENDING,
+  BUFFER_STATUS_FINISHED
+};
+
+struct gbm_bo_buffer
+{
+  struct gbm_bo *bo;
+  xcb_pixmap_t pixmap;
+  enum buffer_status status;
+  struct gbm_bo_buffer *next;
+};
+
+static struct gbm_bo_buffer *buffers = NULL;
+
+static void
+destroy_gbm_bo_buffer(struct gbm_bo *bo, void *data)
+{
+  struct gbm_bo_buffer *b = buffers;
+  struct gbm_bo_buffer *prev = buffers;
+  struct gbm_bo_buffer *next = buffers;
+
+  while (b && b != data)
+  {
+    prev = b;
+    b = b->next;
+    next = b;
+  }
+
+  if (prev)
+    prev->next = next;
+
+  free(data);
+}
+
+static int
+release_gbm_bo_buffers()
+{
+  struct gbm_bo_buffer *b = buffers;
+  int released = 0;
+
+  while (b)
+  {
+    if (b->status == BUFFER_STATUS_FINISHED)
+    {
+      gbm_surface_release_buffer(gbm_surface, b->bo);
+      b->status = BUFFER_STATUS_FREE;
+      released = 1;
+    }
+
+    b = b->next;
+  }
+
+  return released;
+}
+
+static struct gbm_bo_buffer *
+create_gbm_bo_buffer(xcb_connection_t *xcb_conn, struct gbm_bo *bo)
+{
+  struct gbm_bo_buffer *b;
+  struct gbm_bo_buffer *new_b = calloc(sizeof(struct gbm_bo_buffer), 1);
+
+  if (!buffers)
+    buffers = new_b;
+  else
+  {
+    b = buffers;
+
+    while (b->next)
+      b = b->next;
+
+    b->next = new_b;
+  }
+
+  b = new_b;
+
+  xcb_pixmap_t pixmap = xcb_generate_id(xcb_conn);
+  xcb_void_cookie_t pixmap_cookie =
+      xcb_dri3_pixmap_from_buffer_checked(xcb_conn, pixmap, _win, 0,
+                                          gbm_bo_get_width(bo),
+                                          gbm_bo_get_height(bo),
+                                          gbm_bo_get_stride(bo),
+                                          24,
+                                          gbm_bo_get_bpp(bo),
+                                          gbm_bo_get_fd(bo));
+  xcb_generic_error_t *error;
+
+  if ((error = xcb_request_check(xcb_conn, pixmap_cookie)))
+    printf("create pixmap failed\n");
+
+  b->pixmap = pixmap;
+  b->bo = bo;
+
+  gbm_bo_set_user_data(bo, b, destroy_gbm_bo_buffer);
+
+  return b;
+}
+
+static struct gbm_bo_buffer *
+find_gbm_bo_buffer(xcb_pixmap_t pixmap)
+{
+  struct gbm_bo_buffer *b = buffers;
+
+  while (b && b->pixmap != pixmap)
+    b = b->next;
+
+  return b;
+}
+
+static void
+handle_special_event(xcb_present_generic_event_t *ge)
+{
+  switch (ge->evtype)
+  {
+    case XCB_PRESENT_COMPLETE_NOTIFY:
+    {
+      /*
+      xcb_present_complete_notify_event_t *ce = (xcb_present_complete_notify_event_t*) ge;
+
+
+      DBG("XCB_PRESENT_COMPLETE_NOTIFY %u, %s, msc %llu, ust %llu", ce->serial,
+          get_complete_mode_str(ce->mode),
+          (unsigned long long)ce->msc, (unsigned long long)ce->ust);
+      */
+      break;
+    }
+
+    case XCB_PRESENT_EVENT_IDLE_NOTIFY:
+    {
+      xcb_present_idle_notify_event_t *ie =
+          (xcb_present_idle_notify_event_t *)ge;
+      struct gbm_bo_buffer *b = find_gbm_bo_buffer(ie->pixmap);
+
+      assert(b->status == BUFFER_STATUS_PENDING);
+      if (b)
+        b->status = BUFFER_STATUS_FINISHED;
+
+      break;
+    }
+
+    case XCB_PRESENT_EVENT_CONFIGURE_NOTIFY:
+    {
+      /*
+      __attribute__((unused))
+          xcb_present_configure_notify_event_t *ce = (xcb_present_configure_notify_event_t*) ge;
+      drawable->size_changed = true;
+      */
+      break;
+    }
+
+    case XCB_PRESENT_EVENT_REDIRECT_NOTIFY:
+    {
+      /*
+      __attribute__((unused))
+          xcb_present_redirect_notify_event_t *re = (xcb_present_redirect_notify_event_t*) ge;
+      DBG("XCB_PRESENT_EVENT_REDIRECT_NOTIFY %u", re->serial);
+     */
+      break;
+    }
+
+  }
+
+  free(ge);
+}
+static void
+poll_special_events(struct xcb_connection_t *xcb_conn)
+{
+  xcb_generic_event_t *ev;
+
+  while ((ev = xcb_poll_for_special_event(xcb_conn, special_ev)) != NULL)
+  {
+    xcb_present_generic_event_t *ge = (void *)ev;
+    handle_special_event(ge);
+  }
+}
+
+static void
+wait_special_event(struct xcb_connection_t *xcb_conn)
+{
+  xcb_generic_event_t *ev;
+
+  ev = xcb_wait_for_special_event(xcb_conn, special_ev);
+  handle_special_event((xcb_present_generic_event_t*)ev);
+
+  poll_special_events(xcb_conn);
 }
 
 EGLAPI EGLBoolean EGLAPIENTRY
 eglSwapBuffers(EGLDisplay dpy, EGLSurface surface)
 {
-  static struct gbm_bo *prev_bo = 0;
-  xcb_generic_error_t *error;
-
   if (!_eglSwapBuffers(dpy, surface))
     return EGL_FALSE;
 
-  struct gbm_bo *bo = gbm_surface_lock_front_buffer(gbm_surface);
-
-  xcb_pixmap_t *pixmap_data = gbm_bo_get_user_data(bo);
-  xcb_pixmap_t pixmap = 0;
-
-  if (pixmap_data)
-    pixmap = *pixmap_data;
-
   xcb_connection_t *xcb_conn = XGetXCBConnection(x_dpy);
 
-  if (!pixmap)
-  {
-    pixmap = xcb_generate_id(xcb_conn);
-    xcb_void_cookie_t pixmap_cookie =
-        xcb_dri3_pixmap_from_buffer_checked(xcb_conn, pixmap, _win, 0,
-                                            gbm_bo_get_width(bo),
-                                            gbm_bo_get_height(bo),
-                                            gbm_bo_get_stride(bo),
-                                            24,
-                                            gbm_bo_get_bpp(bo),
-                                            gbm_bo_get_fd(bo));
+  poll_special_events(xcb_conn);
 
-    if ((error = xcb_request_check(xcb_conn, pixmap_cookie)))
-      printf("create pixmap failed\n");
+  struct gbm_bo *bo = gbm_surface_lock_front_buffer(gbm_surface);
+  struct gbm_bo_buffer *b = gbm_bo_get_user_data(bo);
 
-    pixmap_data = malloc(sizeof(xcb_pixmap_t));
-    *pixmap_data = pixmap;
-
-    gbm_bo_set_user_data(bo, pixmap_data, destroy_pixmap_data);
-  }
-
+  xcb_generic_error_t *error;
   uint32_t options = XCB_PRESENT_OPTION_NONE;
+
+  if (!b)
+    b = create_gbm_bo_buffer(xcb_conn, bo);
+
+  assert(b->status == BUFFER_STATUS_FREE);
+
+  b->status = BUFFER_STATUS_PENDING;
+
   xcb_void_cookie_t cookie = xcb_present_pixmap_checked(xcb_conn,
                                                         _win,
-                                                        pixmap,
+                                                        b->pixmap,
                                                         0,
                                                         0, 0, 0, 0, // valid, update, x_off, y_off
                                                         None, /* target_crtc */
@@ -300,14 +495,17 @@ eglSwapBuffers(EGLDisplay dpy, EGLSurface surface)
                                                         NULL); /* notifiers */
 
   if ((error = xcb_request_check(xcb_conn, cookie)))
-          printf("present pixmap failed\n");
+    printf("present pixmap failed\n");
 
   xcb_flush(xcb_conn);
 
-  if (prev_bo)
-    gbm_surface_release_buffer(gbm_surface, prev_bo);
+  release_gbm_bo_buffers();
 
-  prev_bo = bo;
+  while (!gbm_surface_has_free_buffers(gbm_surface))
+  {
+    wait_special_event(xcb_conn);
+    release_gbm_bo_buffers();
+  }
 
   return EGL_TRUE;
 }
